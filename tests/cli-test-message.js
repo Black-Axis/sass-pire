4684188d608adec6dde8eca82bac4c1e53732f14
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2026-06-06 15:28:19 +0300

    refactor: remove unneeded files & scripts in `package.json` in main repo

--- tests/cli-test-message.js
+++
@@ -1,4 +0,0 @@
-const chalk = require("chalk");
-
-console.log(chalk.red(`🔴 Please, make sure to make ($test-environment) variable is set to true.`));
-console.log(chalk.red(`🔴 You will find the variable in path: src/abstract/_config.scss.`));
